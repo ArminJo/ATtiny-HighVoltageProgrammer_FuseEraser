2a91d0af41c7b8dad5b87b041ad7814b1bac92df
Armin <armin.joachimsmeyer@gmail.com>
2019-06-15 20:55:36 +0200

    Loop bug fixed

--- src/HVProgrammer.cpp
+++ src/HVProgrammer.cpp
@@ -89,213 +89,214 @@ void loop() {
      * Wait until button pressed or serial available
      */
     char tReceivedChar = 0; // Default value taken, if button pressed
-    if (!digitalRead(START_BUTTON_PIN) || Serial.available() > 0) {
-        // read command
-        if (Serial.available() > 0) {
-            tReceivedChar = Serial.read();
+    while (digitalRead(START_BUTTON_PIN) && Serial.available() == 0) {
+        delay(1); // just wait here
+    }
+    // read command
+    if (Serial.available() > 0) {
+        tReceivedChar = Serial.read();
+    }
+
+    // wait for serial buffer to receive CR/LF and consume it
+    delay(100);
+    while (Serial.available() > 0) {
+        Serial.read();
+    }
+
+    // signal start of programming
+    digitalWrite(LED_BUILTIN, HIGH);
+    pinMode(SDO, OUTPUT); // Set SDO to output
+    digitalWrite(SDI, LOW);
+    digitalWrite(SII, LOW);
+    digitalWrite(SDO, LOW);
+    digitalWrite(RST, HIGH); // 12v Off
+    digitalWrite(VCC, HIGH); // Vcc On
+    delayMicroseconds(20);
+    digitalWrite(RST, LOW); // 12v On
+    delayMicroseconds(10);
+    pinMode(SDO, INPUT); // Set SDO to input
+    delayMicroseconds(300);
+    Serial.println("Reading signature from connected ATtiny...");
+    uint16_t tSignature = readSignature();
+    Serial.println("Reading complete..");
+    Serial.print("Signature is: ");
+    Serial.println(tSignature, HEX);
+
+    readFuses();
+
+    if (tReceivedChar == 'e' || tReceivedChar == 'E') {
+        eraseFlashAndLockBits();
+    }
+
+    if (tSignature == ATTINY13) {
+
+        Serial.println("The ATtiny is detected as ATtiny13/ATtiny13A.");
+        if (tReceivedChar != 'e' && tReceivedChar != 'E' && tReceivedChar != 'f' && tReceivedChar != 'F') {
+            Serial.println("Write LFUSE: 0x6A");
+            writeFuse(LFUSE, 0x6A);
+            Serial.println("Write HFUSE: 0xFF");
+            writeFuse(HFUSE, 0xFF);
+            Serial.println("");
         }
+    } else if (tSignature == ATTINY24 || tSignature == ATTINY44 || tSignature == ATTINY84 || tSignature == ATTINY25
+            || tSignature == ATTINY45 || tSignature == ATTINY85) {
 
-        // wait for serial buffer to receive CR/LF and consume it
-        delay(100);
-        while (Serial.available() > 0) {
-            Serial.read();
+        Serial.print("The ATtiny is detected as ");
+        if (tSignature == ATTINY24)
+            Serial.println("ATTINY24.");
+        else if (tSignature == ATTINY44)
+            Serial.println("ATTINY44.");
+        else if (tSignature == ATTINY84)
+            Serial.println("ATTINY84.");
+        else if (tSignature == ATTINY25)
+            Serial.println("ATTINY25.");
+        else if (tSignature == ATTINY45)
+            Serial.println("ATTINY45.");
+        else if (tSignature == ATTINY85)
+            Serial.println("ATTINY85.");
+
+        if (tReceivedChar != 'e' && tReceivedChar != 'E' && tReceivedChar != 'f' && tReceivedChar != 'F') {
+            Serial.println("Write LFUSE: 0x62");
+            writeFuse(LFUSE, 0x62);
+            Serial.println("Write HFUSE: 0xDF");
+            writeFuse(HFUSE, 0xDF);
+            Serial.println("Write EFUSE: 0xFF");
+            writeFuse(EFUSE, 0xFF);
         }
-
-        // signal start of programming
-        digitalWrite(LED_BUILTIN, HIGH);
-        pinMode(SDO, OUTPUT); // Set SDO to output
-        digitalWrite(SDI, LOW);
-        digitalWrite(SII, LOW);
-        digitalWrite(SDO, LOW);
-        digitalWrite(RST, HIGH); // 12v Off
-        digitalWrite(VCC, HIGH); // Vcc On
-        delayMicroseconds(20);
-        digitalWrite(RST, LOW); // 12v On
-        delayMicroseconds(10);
-        pinMode(SDO, INPUT); // Set SDO to input
-        delayMicroseconds(300);
-        Serial.println("Reading signature from connected ATtiny...");
-        uint16_t tSignature = readSignature();
-        Serial.println("Reading complete..");
-        Serial.print("Signature is: ");
-        Serial.println(tSignature, HEX);
-
-        readFuses();
-
-        if (tReceivedChar == 'e' || tReceivedChar == 'E') {
-            eraseFlashAndLockBits();
-        }
-
-        if (tSignature == ATTINY13) {
-
-            Serial.println("The ATtiny is detected as ATtiny13/ATtiny13A.");
-            if (tReceivedChar != 'e' && tReceivedChar != 'E' && tReceivedChar != 'f' && tReceivedChar != 'F') {
-                Serial.println("Write LFUSE: 0x6A");
-                writeFuse(LFUSE, 0x6A);
-                Serial.println("Write HFUSE: 0xFF");
-                writeFuse(HFUSE, 0xFF);
-                Serial.println("");
-            }
-        } else if (tSignature == ATTINY24 || tSignature == ATTINY44 || tSignature == ATTINY84 || tSignature == ATTINY25
-                || tSignature == ATTINY45 || tSignature == ATTINY85) {
-
-            Serial.print("The ATtiny is detected as ");
-            if (tSignature == ATTINY24)
-                Serial.println("ATTINY24.");
-            else if (tSignature == ATTINY44)
-                Serial.println("ATTINY44.");
-            else if (tSignature == ATTINY84)
-                Serial.println("ATTINY84.");
-            else if (tSignature == ATTINY25)
-                Serial.println("ATTINY25.");
-            else if (tSignature == ATTINY45)
-                Serial.println("ATTINY45.");
-            else if (tSignature == ATTINY85)
-                Serial.println("ATTINY85.");
-
-            if (tReceivedChar != 'e' && tReceivedChar != 'E' && tReceivedChar != 'f' && tReceivedChar != 'F') {
-                Serial.println("Write LFUSE: 0x62");
-                writeFuse(LFUSE, 0x62);
-                Serial.println("Write HFUSE: 0xDF");
-                writeFuse(HFUSE, 0xDF);
-                Serial.println("Write EFUSE: 0xFF");
-                writeFuse(EFUSE, 0xFF);
-            }
-        } else {
-            //Wait for button to release
-            while (!digitalRead(START_BUTTON_PIN))
-                ;
-            delay(100); // debouncing
-            Serial.println("No valid ATtiny signature detected! Try again.");
-            Serial.println();
-            // try again
-            return;
-        }
-        if (tReceivedChar != 'r' && tReceivedChar != 'R') {
-            Serial.println("Fuses will be read again to check if it's changed successfully...");
-            readFuses();
-        }
-
-        digitalWrite(SCI, LOW);
-        digitalWrite(VCC, LOW); // Vcc Off
-        digitalWrite(RST, HIGH); // 12v Off
-
+    } else {
+        //Wait for button to release
+        while (!digitalRead(START_BUTTON_PIN))
+            ;
+        delay(100); // debouncing
+        Serial.println("No valid ATtiny signature detected! Try again.");
         Serial.println();
-        delay(1000);
-        digitalWrite(LED_BUILTIN, LOW);
-        delay(1000);
+        // try again
+        return;
+    }
+    if (tReceivedChar != 'r' && tReceivedChar != 'R') {
+        Serial.println("Fuses will be read again to check if it's changed successfully...");
+        readFuses();
+    }
 
+    digitalWrite(SCI, LOW);
+    digitalWrite(VCC, LOW); // Vcc Off
+    digitalWrite(RST, HIGH); // 12v Off
+
+    Serial.println();
+    delay(1000);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(1000);
+
+    /*
+     * If controlled by serial input enable a new run, otherwise blink forever
+     */
+    if (tReceivedChar == 0) {
         /*
-         * If controlled by serial input enable a new run, otherwise blink forever
+         * Blink forever after end of programming triggered by button
          */
-        if (tReceivedChar == 0) {
-            /*
-             * Blink forever after end of programming triggered by button
-             */
-            while (true) {
-                delay(150);
-                digitalWrite(LED_BUILTIN, HIGH);
-                delay(50);
-                digitalWrite(LED_BUILTIN, LOW);
-            }
-        } else {
-            Serial.println();
-            Serial.println("Programming finished, allow a new run.");
+        while (true) {
+            delay(150);
+            digitalWrite(LED_BUILTIN, HIGH);
+            delay(50);
+            digitalWrite(LED_BUILTIN, LOW);
         }
+    } else {
+        Serial.println();
+        Serial.println("Programming finished, allow a new run.");
     }
 }
 
 uint8_t shiftOut(uint8_t aValue, uint8_t aAddress) {
-    uint16_t tInBits = 0;
+uint16_t tInBits = 0;
 
-    //Wait with timeout until SDO goes high
-    uint32_t tMillis = millis();
-    while (!digitalRead(SDO)) {
-        if (millis() > (tMillis + READING_TIMEOUT_MILLIS)) {
-            break;
-        }
+//Wait with timeout until SDO goes high
+uint32_t tMillis = millis();
+while (!digitalRead(SDO)) {
+    if (millis() > (tMillis + READING_TIMEOUT_MILLIS)) {
+        break;
     }
-    uint16_t tSDIOut = (uint16_t) aValue << 2;
-    uint16_t tSIIOut = (uint16_t) aAddress << 2;
-    for (int8_t i = 10; i >= 0; i--) {
-        digitalWrite(SDI, !!(tSDIOut & (1 << i)));
-        digitalWrite(SII, !!(tSIIOut & (1 << i)));
-        tInBits <<= 1;
-        tInBits |= digitalRead(SDO);
-        digitalWrite(SCI, HIGH);
-        digitalWrite(SCI, LOW);
-    }
-    return tInBits >> 2;
-    Serial.print(" tInBits=");
-    Serial.println(tInBits);
+}
+uint16_t tSDIOut = (uint16_t) aValue << 2;
+uint16_t tSIIOut = (uint16_t) aAddress << 2;
+for (int8_t i = 10; i >= 0; i--) {
+    digitalWrite(SDI, !!(tSDIOut & (1 << i)));
+    digitalWrite(SII, !!(tSIIOut & (1 << i)));
+    tInBits <<= 1;
+    tInBits |= digitalRead(SDO);
+    digitalWrite(SCI, HIGH);
+    digitalWrite(SCI, LOW);
+}
+return tInBits >> 2;
+Serial.print(" tInBits=");
+Serial.println(tInBits);
 
 }
 
 void eraseFlashAndLockBits() {
 
-    Serial.println("Erasing flash and lock bits...");
-    shiftOut(0x80, 0x4C);
-    shiftOut(0x00, 0x64);
-    shiftOut(0x00, 0x6C);
+Serial.println("Erasing flash and lock bits...");
+shiftOut(0x80, 0x4C);
+shiftOut(0x00, 0x64);
+shiftOut(0x00, 0x6C);
 
-    //Wait with timeout until SDO goes high
-    uint32_t tMillis = millis();
-    while (!digitalRead(SDO)) {
-        if (millis() > (tMillis + READING_TIMEOUT_MILLIS)) {
-            break;
-        }
+//Wait with timeout until SDO goes high
+uint32_t tMillis = millis();
+while (!digitalRead(SDO)) {
+    if (millis() > (tMillis + READING_TIMEOUT_MILLIS)) {
+        break;
     }
+}
 }
 
 void writeFuse(uint16_t aFuseAddress, uint8_t aFuseValue) {
 
-    Serial.print("Writing fuse value ");
-    Serial.print(aFuseValue, HEX);
-    Serial.println(" to ATtiny...");
+Serial.print("Writing fuse value ");
+Serial.print(aFuseValue, HEX);
+Serial.println(" to ATtiny...");
 
-    shiftOut(0x40, 0x4C);
-    shiftOut(aFuseValue, 0x2C);
-    shiftOut(0x00, (uint8_t) (aFuseAddress >> 8));
-    shiftOut(0x00, (uint8_t) aFuseAddress);
+shiftOut(0x40, 0x4C);
+shiftOut(aFuseValue, 0x2C);
+shiftOut(0x00, (uint8_t) (aFuseAddress >> 8));
+shiftOut(0x00, (uint8_t) aFuseAddress);
 
-    Serial.println("Writing complete.");
+Serial.println("Writing complete.");
 }
 
 void readFuses() {
 
-    Serial.println("Reading fuse settings from ATtiny...");
+Serial.println("Reading fuse settings from ATtiny...");
 
-    uint8_t tValue;
-    shiftOut(0x04, 0x4C); // LFuse
-    shiftOut(0x00, 0x68);
-    tValue = shiftOut(0x00, 0x6C);
-    Serial.print("LFuse: ");
-    Serial.print(tValue, HEX);
+uint8_t tValue;
+shiftOut(0x04, 0x4C); // LFuse
+shiftOut(0x00, 0x68);
+tValue = shiftOut(0x00, 0x6C);
+Serial.print("LFuse: ");
+Serial.print(tValue, HEX);
 
-    shiftOut(0x04, 0x4C); // HFuse
-    shiftOut(0x00, 0x7A);
-    tValue = shiftOut(0x00, 0x7E);
-    Serial.print(", HFuse: ");
-    Serial.print(tValue, HEX);
+shiftOut(0x04, 0x4C); // HFuse
+shiftOut(0x00, 0x7A);
+tValue = shiftOut(0x00, 0x7E);
+Serial.print(", HFuse: ");
+Serial.print(tValue, HEX);
 
-    shiftOut(0x04, 0x4C); // EFuse
-    shiftOut(0x00, 0x6A);
-    tValue = shiftOut(0x00, 0x6E);
-    Serial.print(", EFuse: ");
-    Serial.println(tValue, HEX);
-    Serial.println("Reading complete.");
+shiftOut(0x04, 0x4C); // EFuse
+shiftOut(0x00, 0x6A);
+tValue = shiftOut(0x00, 0x6E);
+Serial.print(", EFuse: ");
+Serial.println(tValue, HEX);
+Serial.println("Reading complete.");
 }
 
 uint16_t readSignature() {
-    uint16_t tSignature = 0;
-    uint8_t tValue;
-    for (uint8_t tIndex = 1; tIndex < 3; tIndex++) {
-        shiftOut(0x08, 0x4C);
-        shiftOut(tIndex, 0x0C);
-        shiftOut(0x00, 0x68);
-        tValue = shiftOut(0x00, 0x6C);
-        tSignature = (tSignature << 8) + tValue;
-    }
+uint16_t tSignature = 0;
+uint8_t tValue;
+for (uint8_t tIndex = 1; tIndex < 3; tIndex++) {
+    shiftOut(0x08, 0x4C);
+    shiftOut(tIndex, 0x0C);
+    shiftOut(0x00, 0x68);
+    tValue = shiftOut(0x00, 0x6C);
+    tSignature = (tSignature << 8) + tValue;
+}
 
-    return tSignature;
+return tSignature;
 }
